ab154b4f44d0d67d668a0b748f8564f1e52ca9c5
riverzzz <455405955@qq.com>
2021-01-05 20:10:28 +0800

    read csv with no header

--- nebula-exchange/src/main/scala/com/vesoft/nebula/exchange/config/SourceConfigs.scala
+++ nebula-exchange/src/main/scala/com/vesoft/nebula/exchange/config/SourceConfigs.scala
@@ -9,33 +9,33 @@ package com.vesoft.nebula.exchange.config
 import com.vesoft.nebula.exchange.utils.NebulaUtils
 
 /**
-  * Category use to explain the data source which the Spark application could reading.
-  */
+ * Category use to explain the data source which the Spark application could reading.
+ */
 object SourceCategory extends Enumeration {
   type Type = Value
 
   val PARQUET = Value("PARQUET")
-  val ORC     = Value("ORC")
-  val JSON    = Value("JSON")
-  val CSV     = Value("CSV")
-  val TEXT    = Value("TEXT")
+  val ORC = Value("ORC")
+  val JSON = Value("JSON")
+  val CSV = Value("CSV")
+  val TEXT = Value("TEXT")
 
-  val HIVE        = Value("HIVE")
-  val NEO4J       = Value("NEO4J")
+  val HIVE = Value("HIVE")
+  val NEO4J = Value("NEO4J")
   val JANUS_GRAPH = Value("JANUS GRAPH")
-  val MYSQL       = Value("MYSQL")
-  val HBASE       = Value("HBASE")
+  val MYSQL = Value("MYSQL")
+  val HBASE = Value("HBASE")
 
   val SOCKET = Value("SOCKET")
-  val KAFKA  = Value("KAFKA")
+  val KAFKA = Value("KAFKA")
   val PULSAR = Value("PULSAR")
 }
 
 class SourceCategory
 
 /**
-  * DataSourceConfigEntry
-  */
+ * DataSourceConfigEntry
+ */
 sealed trait DataSourceConfigEntry {
   def category: SourceCategory.Value
 }
@@ -53,31 +53,33 @@ sealed trait StreamingDataSourceConfigEntry extends DataSourceConfigEntry {
 }
 
 /**
-  * FileBaseSourceConfigEntry
-  *
-  * @param category
-  * @param path
-  * @param separator
-  * @param header
-  */
+ * FileBaseSourceConfigEntry
+ *
+ * @param category
+ * @param path
+ * @param separator
+ * @param header
+ * @param csvFields
+ */
 case class FileBaseSourceConfigEntry(override val category: SourceCategory.Value,
                                      override val path: String,
                                      separator: Option[String] = None,
-                                     header: Option[Boolean] = None)
-    extends FileDataSourceConfigEntry {
+                                     header: Option[Boolean] = None,
+                                     csvFields: Option[List[String]] = None)
+  extends FileDataSourceConfigEntry {
   override def toString: String = {
-    s"File source path: ${path}, separator: ${separator}, header: ${header}"
+    s"File source path: ${path}, separator: ${separator}, header: ${header}, csvFields: ${csvFields}"
   }
 }
 
 /**
-  * HiveSourceConfigEntry
-  *
-  * @param sentence
-  */
+ * HiveSourceConfigEntry
+ *
+ * @param sentence
+ */
 case class HiveSourceConfigEntry(override val category: SourceCategory.Value,
                                  override val sentence: String)
-    extends ServerDataSourceConfigEntry {
+  extends ServerDataSourceConfigEntry {
   require(sentence.trim.nonEmpty)
 
   override def toString: String = {
@@ -86,18 +88,18 @@ case class HiveSourceConfigEntry(override val category: SourceCategory.Value,
 }
 
 /**
-  * Neo4JSourceConfigEntry
-  *
-  * @param sentence
-  * @param name
-  * @param server
-  * @param user
-  * @param password
-  * @param database
-  * @param encryption
-  * @param parallel
-  * @param checkPointPath use save resume data dir path.
-  */
+ * Neo4JSourceConfigEntry
+ *
+ * @param sentence
+ * @param name
+ * @param server
+ * @param user
+ * @param password
+ * @param database
+ * @param encryption
+ * @param parallel
+ * @param checkPointPath use save resume data dir path.
+ */
 case class Neo4JSourceConfigEntry(override val category: SourceCategory.Value,
                                   override val sentence: String,
                                   name: String,
@@ -108,7 +110,7 @@ case class Neo4JSourceConfigEntry(override val category: SourceCategory.Value,
                                   encryption: Boolean,
                                   parallel: Int,
                                   checkPointPath: Option[String])
-    extends ServerDataSourceConfigEntry {
+  extends ServerDataSourceConfigEntry {
   require(sentence.trim.nonEmpty && user.trim.nonEmpty && parallel > 0)
 
   override def toString: String = {
@@ -120,24 +122,24 @@ case class Neo4JSourceConfigEntry(override val category: SourceCategory.Value,
 case class JanusGraphSourceConfigEntry(override val category: SourceCategory.Value,
                                        override val sentence: String,
                                        isEdge: Boolean)
-    extends ServerDataSourceConfigEntry {
+  extends ServerDataSourceConfigEntry {
   override def toString: String = {
     s"Janus graph source"
   }
 }
 
 /**
-  * MySQLSourceConfigEntry
-  *
-  * @param host
-  * @param port
-  * @param database
-  * @param table
-  * @param user
-  * @param password
-  * @param sentence
-  * @return
-  */
+ * MySQLSourceConfigEntry
+ *
+ * @param host
+ * @param port
+ * @param database
+ * @param table
+ * @param user
+ * @param password
+ * @param sentence
+ * @return
+ */
 case class MySQLSourceConfigEntry(override val category: SourceCategory.Value,
                                   host: String,
                                   port: Int,
@@ -146,7 +148,7 @@ case class MySQLSourceConfigEntry(override val category: SourceCategory.Value,
                                   user: String,
                                   password: String,
                                   override val sentence: String)
-    extends ServerDataSourceConfigEntry {
+  extends ServerDataSourceConfigEntry {
   require(
     host.trim.length != 0 && port > 0 && database.trim.length > 0 && table.trim.length > 0 && user.trim.length > 0)
 
@@ -157,16 +159,16 @@ case class MySQLSourceConfigEntry(override val category: SourceCategory.Value,
 }
 
 /**
-  * TODO: Support more config item about Kafka Consumer
-  *
-  * @param server
-  * @param topic
-  */
+ * TODO: Support more config item about Kafka Consumer
+ *
+ * @param server
+ * @param topic
+ */
 case class KafkaSourceConfigEntry(override val category: SourceCategory.Value,
                                   override val intervalSeconds: Int,
                                   server: String,
                                   topic: String)
-    extends StreamingDataSourceConfigEntry {
+  extends StreamingDataSourceConfigEntry {
   require(server.trim.nonEmpty && topic.trim.nonEmpty)
 
   override def toString: String = {
@@ -175,19 +177,19 @@ case class KafkaSourceConfigEntry(override val category: SourceCategory.Value,
 }
 
 /**
-  * PulsarSourceConfigEntry
-  *
-  * @param serviceUrl
-  * @param adminUrl use to get data schema.
-  * @param options
-  * @return
-  */
+ * PulsarSourceConfigEntry
+ *
+ * @param serviceUrl
+ * @param adminUrl use to get data schema.
+ * @param options
+ * @return
+ */
 case class PulsarSourceConfigEntry(override val category: SourceCategory.Value,
                                    override val intervalSeconds: Int,
                                    serviceUrl: String,
                                    adminUrl: String,
                                    options: Map[String, String])
-    extends StreamingDataSourceConfigEntry {
+  extends StreamingDataSourceConfigEntry {
   require(serviceUrl.trim.nonEmpty && adminUrl.trim.nonEmpty && intervalSeconds >= 0)
   require(options.keys.count(key => List("topic", "topics", "topicsPattern").contains(key)) == 1)
 
@@ -197,16 +199,16 @@ case class PulsarSourceConfigEntry(override val category: SourceCategory.Value,
 }
 
 /**
-  * HBaseSourceConfigEntry
-  *
-  */
+ * HBaseSourceConfigEntry
+ *
+ */
 case class HBaseSourceConfigEntry(override val category: SourceCategory.Value,
                                   host: String,
                                   port: String,
                                   table: String,
                                   columnFamily: String,
                                   fields: List[String])
-    extends ServerDataSourceConfigEntry() {
+  extends ServerDataSourceConfigEntry() {
 
   require(host.trim.length != 0 && port.trim.length != 0 && NebulaUtils
     .isNumic(port.trim) && table.trim.length > 0 && table.trim.length > 0 && columnFamily.trim.length > 0)
--- nebula-exchange/src/main/scala/com/vesoft/nebula/exchange/reader/FileBaseReader.scala
+++ nebula-exchange/src/main/scala/com/vesoft/nebula/exchange/reader/FileBaseReader.scala
@@ -12,11 +12,11 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 /**
-  * The FileBaseReader is the abstract class for HDFS file reader.
-  *
-  * @param session
-  * @param path
-  */
+ * The FileBaseReader is the abstract class for HDFS file reader.
+ *
+ * @param session
+ * @param path
+ */
 abstract class FileBaseReader(val session: SparkSession, val path: String) extends Reader {
 
   require(path.trim.nonEmpty)
@@ -27,13 +27,13 @@ abstract class FileBaseReader(val session: SparkSession, val path: String) exten
 }
 
 /**
-  * The ParquetReader extend the FileBaseReader and support read parquet file from HDFS.
-  *
-  * @param session
-  * @param parquetConfig
-  */
+ * The ParquetReader extend the FileBaseReader and support read parquet file from HDFS.
+ *
+ * @param session
+ * @param parquetConfig
+ */
 class ParquetReader(override val session: SparkSession, parquetConfig: FileBaseSourceConfigEntry)
-    extends FileBaseReader(session, parquetConfig.path) {
+  extends FileBaseReader(session, parquetConfig.path) {
 
   override def read(): DataFrame = {
     session.read.parquet(path)
@@ -41,13 +41,13 @@ class ParquetReader(override val session: SparkSession, parquetConfig: FileBaseS
 }
 
 /**
-  * The ORCReader extend the FileBaseReader and support read orc file from HDFS.
-  *
-  * @param session
-  * @param orcConfig
-  */
+ * The ORCReader extend the FileBaseReader and support read orc file from HDFS.
+ *
+ * @param session
+ * @param orcConfig
+ */
 class ORCReader(override val session: SparkSession, orcConfig: FileBaseSourceConfigEntry)
-    extends FileBaseReader(session, orcConfig.path) {
+  extends FileBaseReader(session, orcConfig.path) {
 
   override def read(): DataFrame = {
     session.read.orc(path)
@@ -55,13 +55,13 @@ class ORCReader(override val session: SparkSession, orcConfig: FileBaseSourceCon
 }
 
 /**
-  * The JSONReader extend the FileBaseReader and support read json file from HDFS.
-  *
-  * @param session
-  * @param jsonConfig
-  */
+ * The JSONReader extend the FileBaseReader and support read json file from HDFS.
+ *
+ * @param session
+ * @param jsonConfig
+ */
 class JSONReader(override val session: SparkSession, jsonConfig: FileBaseSourceConfigEntry)
-    extends FileBaseReader(session, jsonConfig.path) {
+  extends FileBaseReader(session, jsonConfig.path) {
 
   override def read(): DataFrame = {
     session.read.json(path)
@@ -69,40 +69,41 @@ class JSONReader(override val session: SparkSession, jsonConfig: FileBaseSourceC
 }
 
 /**
-  * The CSVReader extend the FileBaseReader and support read csv file from HDFS.
-  * All types of the structure are StringType.
-  *
-  * @param session
-  * @param csvConfig
-  */
+ * The CSVReader extend the FileBaseReader and support read csv file from HDFS.
+ * All types of the structure are StringType.
+ *
+ * @param session
+ * @param csvConfig
+ */
 class CSVReader(override val session: SparkSession, csvConfig: FileBaseSourceConfigEntry)
-    extends FileBaseReader(session, csvConfig.path) {
+  extends FileBaseReader(session, csvConfig.path) {
 
   override def read(): DataFrame = {
-    session.read
+    val df = session.read
       .option("delimiter", csvConfig.separator.get)
       .option("header", csvConfig.header.get)
       .option("inferSchema", true)
       .csv(path)
+    csvConfig.csvFields.map(df.toDF(_: _*)).getOrElse(df)
   }
 }
 
 /**
-  * The CustomReader extend the FileBaseReader and support read text file from HDFS.
-  * Transformation is a function convert a line into Row.
-  * The structure of the row should be specified.
-  *
-  * @param session
-  * @param customConfig
-  * @param transformation
-  * @param structType
-  */
+ * The CustomReader extend the FileBaseReader and support read text file from HDFS.
+ * Transformation is a function convert a line into Row.
+ * The structure of the row should be specified.
+ *
+ * @param session
+ * @param customConfig
+ * @param transformation
+ * @param structType
+ */
 abstract class CustomReader(override val session: SparkSession,
                             customConfig: FileBaseSourceConfigEntry,
                             transformation: String => Row,
                             filter: Row => Boolean,
                             structType: StructType)
-    extends FileBaseReader(session, customConfig.path) {
+  extends FileBaseReader(session, customConfig.path) {
 
   override def read(): DataFrame = {
     val encoder = RowEncoder.apply(structType)
